08fc9e5cadfe2a3a2075def7f8e11034a50cba2b
Vibhanshu Jain <cs19b1027@iiitr.ac.in>
2020-11-26 14:37:52 +0530

    Some news updates

--- Data Structure 2/Assignment/A4/queue.c
+++ Data Structure 2/Assignment/A4/queue.c
@@ -22,7 +22,64 @@ struct Q_node* new_node_queue( int k )
     return temp;
 }
 
-struct Queue* create_queue()
+struct queue* create_queue()
 {
-    struct
+    struct queue* temp = (struct queue*)malloc(sizeof(struct queue));
+    temp->front = NULL;
+    temp->rear = NULL;
+    return temp;
+}
+
+void enqueue( struct queue* Queue, int k )
+{
+    struct Q_node* temp = new_node_queue(k);
+
+    if(Queue->rear == NULL)
+    {
+        Queue->front = Queue->rear = temp;
+        return;
+    }
+
+    Queue->rear->next = temp;
+    temp->prev = Queue->rear;
+    Queue->rear = temp;
+    return;
+}
+
+void dequeue(struct queue* Queue)
+{
+    if(Queue->front == NULL)
+        return;
+
+    struct Q_node* temp = Queue->front;
+
+    Queue->front = Queue->front->next;
+
+    if(Queue->front == NULL)
+        Queue->rear = NULL;
+    else
+        Queue->front->prev = NULL;
+
+    free(temp);
+    return;
+}
+
+
+int main()
+{
+    struct queue* q = create_queue();
+    enqueue(q, 10);
+    enqueue(q, 20);
+    dequeue(q);
+    dequeue(q);
+    enqueue(q, 30);
+    enqueue(q, 40);
+    enqueue(q, 50);
+    dequeue(q);
+    dequeue(q);
+    dequeue(q);
+    printf("Queue Front : %d \n", q->front->data);
+    printf("Queue Rear : %d", q->rear->data);
+
+    return 0;
 }
