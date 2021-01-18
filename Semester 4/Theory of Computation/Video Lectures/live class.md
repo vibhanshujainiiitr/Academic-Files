5b5d0463ecd0a351db1076b8627cbc31e22c8529
vibhanshujainiiitr <cs19b1027@iiitr.ac.in>
2021-01-18 07:47:38 +0530

    Some news updates

---
+++ Semester 4/Theory of Computation/Video Lectures/live class.md
@@ -0,0 +1,8 @@
+# Live Class 
+## By Subruk Sir
+
+## [Lecture 1 - Jan 11](https://www.youtube.com/watch?v=nvePMvdEeC0)
+
+## [Lecture 2 - Jan 13](https://www.youtube.com/watch?v=R5erh-a17Z8)
+
+
--- gitadd.sh
+++ gitadd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /home/the_looser/Desktop/Academics/ && git add --force --all :/ && git commit -m 'Some news updates' && git push --recurse-submodules=on-demand --set-upstream origin master --force 
+cd /home/vibhanshu/Desktop/Academic-Files && git add --force --all :/ && git commit -m 'Some news updates' && git push --recurse-submodules=on-demand --set-upstream origin master --force 
 # && git reflog expire --all --expire=now && git gc --prune=now --aggressive
