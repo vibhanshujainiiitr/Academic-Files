1331f5eac47abaa3375d492fb0598abce189dc46
Vibhanshu <cs19b1027@iiitr.ac.in>
2021-03-31 14:13:05 +0530

    Some news updates

--- gitadd.sh
+++ gitadd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /home/vibhanshu/Desktop/Academic-Files && git add --force --all :/ && git commit -m 'Some news updates' && git push --recurse-submodules=on-demand --set-upstream origin master --force 
+cd ~/run/media/vibhanshu/New Volume/Academic-Files && git add --force --all :/ && git commit -m 'Some news updates' && git push --recurse-submodules=on-demand --set-upstream origin master --force 
 # && git reflog expire --all --expire=now && git gc --prune=now --aggressive
